2e024bd8f30adfafe674dfb0b7df4fae5ba1f405
Mahi-Mani <m.mahishamani@gmail.com>
2025-08-14 15:06:06 -0700

    Active state for clickable buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,3 +12,14 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.animate-on-scroll').forEach((el) => {
     observer.observe(el);
 });
+
+let navbar = document.getElementsByClassName("tabs");
+Array.from(navbar[0].children).forEach((item) => {
+    item.addEventListener("click", function () {
+        Array.from(navbar[0].children).forEach(node => {
+            node.classList.remove("active");
+        });
+        item.classList.add("active");
+        console.log("I am clicked");
+    });
+});
